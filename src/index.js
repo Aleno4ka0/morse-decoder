0c82ce3d55e40d00e6033990228055520c18ff49
Aleno4ka0 <vselenskoyezlo@gmail.com>
2020-10-11 14:25:26 +0400

    feat: add morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function findSymbols(value){
+    return Object.keys(MORSE_TABLE).find(key => MORSE_TABLE[key] === value)
+}
+
 function decode(expr) {
-    // write your solution here
+    var numStr = expr.split(/(.{10})/).filter(O=>O)
+    var numDecode = []
+    var result = ''
+
+    for(var i = 0; i < numStr.length; i++){
+        console.log(numStr[i]);
+        if(numStr[i] == '**********'){
+            result += ' '
+        }else{
+            numDecode = (numStr[i].split(/(.{2})/).filter(O=>O))
+            var resDecode = ''
+            for(var j = 0; j < numDecode.length; j++){
+
+                if(numDecode[j] == 10){
+                    resDecode += '.'
+                }
+                if(numDecode[j] == 11){
+                    resDecode += '-'
+                }
+            }
+            result += MORSE_TABLE[resDecode]
+        }
+
+    }
+    return result
 }
 
 module.exports = {
